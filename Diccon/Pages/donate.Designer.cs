9f3f969ea6c50fdf8287938731aabad3c0beccc9
Tran Huu Dang <tranhuudang148@gmail.com>
2022-03-20 21:01:46 +0700

    daily check

--- Diccon/Pages/donate.Designer.cs
+++ Diccon/Pages/donate.Designer.cs
@@ -30,16 +30,16 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(donate));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btOpenQr = new Diccon.RoundedLabel();
+            this.panelQr = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.panelQr = new System.Windows.Forms.Panel();
-            this.btOpenQr = new Diccon.RoundedLabel();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelQr.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -55,16 +55,35 @@
             this.panel1.Size = new System.Drawing.Size(407, 635);
             this.panel1.TabIndex = 1;
             // 
-            // pictureBox2
+            // btOpenQr
             // 
-            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pictureBox2.Image = global::Diccon.Properties.Resources.donate_background;
-            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(407, 244);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.btOpenQr.AutoSize = true;
+            this.btOpenQr.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btOpenQr.BorderColor = System.Drawing.Color.White;
+            this.btOpenQr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btOpenQr.ForeColor = System.Drawing.Color.White;
+            this.btOpenQr.Location = new System.Drawing.Point(109, 382);
+            this.btOpenQr.Name = "btOpenQr";
+            this.btOpenQr.Padding = new System.Windows.Forms.Padding(7);
+            this.btOpenQr.Radius = 20;
+            this.btOpenQr.Size = new System.Drawing.Size(186, 34);
+            this.btOpenQr.TabIndex = 5;
+            this.btOpenQr.Text = "Buy me a cup of coffee";
+            this.btOpenQr.Thickness = 5F;
+            this.btOpenQr.Click += new System.EventHandler(this.btOpenQr_Click);
+            this.btOpenQr.MouseEnter += new System.EventHandler(this.RoundedLabel_MouseEnter);
+            this.btOpenQr.MouseLeave += new System.EventHandler(this.RoundedLabel_MouseLeave);
+            // 
+            // panelQr
+            // 
+            this.panelQr.Controls.Add(this.comboBox1);
+            this.panelQr.Controls.Add(this.pictureBox1);
+            this.panelQr.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelQr.Location = new System.Drawing.Point(0, 368);
+            this.panelQr.Name = "panelQr";
+            this.panelQr.Size = new System.Drawing.Size(407, 267);
+            this.panelQr.TabIndex = 4;
+            this.panelQr.Visible = false;
             // 
             // comboBox1
             // 
@@ -88,6 +107,17 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox2.Image = global::Diccon.Properties.Resources.donate_background;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(407, 244);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
@@ -99,36 +129,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
-            // panelQr
-            // 
-            this.panelQr.Controls.Add(this.comboBox1);
-            this.panelQr.Controls.Add(this.pictureBox1);
-            this.panelQr.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelQr.Location = new System.Drawing.Point(0, 368);
-            this.panelQr.Name = "panelQr";
-            this.panelQr.Size = new System.Drawing.Size(407, 267);
-            this.panelQr.TabIndex = 4;
-            this.panelQr.Visible = false;
-            // 
-            // btOpenQr
-            // 
-            this.btOpenQr.AutoSize = true;
-            this.btOpenQr.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.btOpenQr.BorderColor = System.Drawing.Color.White;
-            this.btOpenQr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btOpenQr.ForeColor = System.Drawing.Color.White;
-            this.btOpenQr.Location = new System.Drawing.Point(112, 382);
-            this.btOpenQr.Name = "btOpenQr";
-            this.btOpenQr.Padding = new System.Windows.Forms.Padding(7);
-            this.btOpenQr.Radius = 20;
-            this.btOpenQr.Size = new System.Drawing.Size(181, 34);
-            this.btOpenQr.TabIndex = 5;
-            this.btOpenQr.Text = "Buy us a cup of coffee";
-            this.btOpenQr.Thickness = 5F;
-            this.btOpenQr.Click += new System.EventHandler(this.btOpenQr_Click);
-            this.btOpenQr.MouseEnter += new System.EventHandler(this.RoundedLabel_MouseEnter);
-            this.btOpenQr.MouseLeave += new System.EventHandler(this.RoundedLabel_MouseLeave);
-            // 
             // donate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -139,9 +139,9 @@
             this.Text = "Donate";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelQr.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
