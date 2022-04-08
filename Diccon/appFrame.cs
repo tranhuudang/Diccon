4a39fe6e78229ca350721e915f6597cacd9f616f
Tran Huu Dang <tranhuudang148@gmail.com>
2022-04-08 08:47:21 +0700

    DPI apply

--- Diccon/appFrame.cs
+++ Diccon/appFrame.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Net;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 using System.Xml;
@@ -25,9 +26,75 @@ namespace Diccon
         {
             InitializeComponent();
         }
+        // Giảm kích thước font chữ
+        public void SetAllControlsFont(Control.ControlCollection ctrls, int minusFontSize)
+        {
+            foreach (Control ctrl in ctrls)
+            {
+                if (ctrl.Controls != null)
+                    SetAllControlsFont(ctrl.Controls, minusFontSize);
+
+                ctrl.Font = new Font("MS Reference Sans Serif", ctrl.Font.Size - minusFontSize);
+
+            }
+        }
+        // Xóa nhòe chữ cho scale trên 100
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+        // #####################################################################################################
+        // Hàm và thủ tục này dùng để kiểm tra mức độ thu phóng của hệ điều hành
+        // Qua đó cho phép người lập trình xử lí giao diện tốt hơn
+        [System.Runtime.InteropServices.DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
+
+        public enum DeviceCap
+        {
+            VERTRES = 10,
+            DESKTOPVERTRES = 117
+        }
+
+        static double GetWindowsScreenScalingFactor(bool percentage = true)
+        {
+            //Create Graphics object from the current windows handle
+            Graphics GraphicsObject = Graphics.FromHwnd(IntPtr.Zero);
+            //Get Handle to the device context associated with this Graphics object
+            IntPtr DeviceContextHandle = GraphicsObject.GetHdc();
+            //Call GetDeviceCaps with the Handle to retrieve the Screen Height
+            int LogicalScreenHeight = GetDeviceCaps(DeviceContextHandle, (int)DeviceCap.VERTRES);
+            int PhysicalScreenHeight = GetDeviceCaps(DeviceContextHandle, (int)DeviceCap.DESKTOPVERTRES);
+            //Divide the Screen Heights to get the scaling factor and round it to two decimals
+            double ScreenScalingFactor = Math.Round((double)PhysicalScreenHeight / (double)LogicalScreenHeight, 2);
+            //If requested as percentage - convert it
+            if (percentage)
+            {
+                ScreenScalingFactor *= 100.0;
+            }
+            //Release the Handle and Dispose of the GraphicsObject object
+            GraphicsObject.ReleaseHdc(DeviceContextHandle);
+            GraphicsObject.Dispose();
+            //Return the Scaling Factor
+            return ScreenScalingFactor;
+        }
+        // #####################################################################################################
+
 
         private void AppFrame_Load(object sender, EventArgs e)
         {
+            switch (GetWindowsScreenScalingFactor())
+            {
+                case 100: // 100% scaling
+
+                    break;
+                case 125: // 125% scaling
+
+                    SetProcessDPIAware();
+                    SetAllControlsFont(this.Controls, 4);
+                    break;
+                case 150: // 150% scaling
+                    SetProcessDPIAware();
+                    SetAllControlsFont(this.Controls, 5);
+                    break;
+            }
             switch (Properties.Settings.Default["staredForm"])
             {
                 case "Dictionary":
